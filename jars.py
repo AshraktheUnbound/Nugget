8a23b2a37fad94771caadbc1e79004712694e000
Ashrak <BlackRebel@mail.com>
2023-04-21 09:38:28 -0500

    Some Cleanup

--- jars.py
+++ jars.py
@@ -34,3 +34,71 @@ def contest():
         for col in range(len(map_key[row])):
 
 #cleanse()
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the window
+WIDTH = 800
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Round Robin Contest")
+
+# Load the images
+images = []
+for i in range(1, 81):
+    image = pygame.image.load(f"{i}.jpg")
+    images.append(image)
+
+# Create a dictionary to keep track of votes
+votes = {}
+for i in range(1, 81):
+    votes[i] = 0
+
+# Set up the font
+font = pygame.font.Font(None, 36)
+
+# Set up the clock
+clock = pygame.time.Clock()
+
+# Main game loop
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Get the position of the mouse click
+            pos = pygame.mouse.get_pos()
+            # Check which image was clicked on
+            if pos[0] < WIDTH // 2:
+                # Left image was clicked
+                votes[current_images[0]] += 1
+            else:
+                # Right image was clicked
+                votes[current_images[1]] += 1
+
+    # Choose two random images
+    current_images = random.sample(range(1, 81), 2)
+
+    # Display the images
+    SCREEN.blit(images[current_images[0] - 1], (0, 0))
+    SCREEN.blit(images[current_images[1] - 1], (WIDTH // 2, 0))
+
+    # Display the vote count
+    vote_text = font.render(f"Votes: {sum(votes.values())}", True, (255, 255, 255))
+    SCREEN.blit(vote_text, (10, 10))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Tick the clock
+    clock.tick(60)
+
+# Print the final results
+print("Results:")
+for i in range(1, 81):
+    print(f"{i}: {votes[i]} votes")
